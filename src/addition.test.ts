e4c1db194ba311fbbef936bd30f5144d47fac20d
platylm <n_nareenart@kkumail.com>
2020-01-10 11:03:40 +0700

    [Edited]: แยก object splitTextToNumber ออกมาเป็น function และเพิ่ม unit test เข้าไป

--- src/addition.test.ts
+++ src/addition.test.ts
@@ -1,27 +1,41 @@
-import addition from './addition'
+import addition, {splitTextToNumber} from './addition'
 
-describe('Addition',() => {
-    it('Input 1plus2plus3plus4 Should be 10',()=>{
-        const expected = "10"
-        const text = "1plus2plus3plus4"
+describe('Name of the group', () => {
 
-        const actual = addition(text)
-        expect(actual).toEqual(expected)
-    });
+    describe('Addition', () => {
+        it('Input 1plus2plus3plus4 Should be 10', () => {
+            const expected = "10"
+            const text = "1plus2plus3plus4"
 
-    it('Input 0plus0 Should be 0',()=>{
-        const expected = "0"
-        const text = "0plus0"
+            const actual = addition(text)
+            expect(actual).toEqual(expected)
+        });
 
-        const actual = addition(text)
-        expect(actual).toEqual(expected)
-    });
+        it('Input 0plus0 Should be 0', () => {
+            const expected = "0"
+            const text = "0plus0"
 
-    it('Input 1plus2plus3minus4 Should be 2',()=>{
-        const expected = "2"
-        const text = "1plus2plus3minus4"
+            const actual = addition(text)
+            expect(actual).toEqual(expected)
+        });
 
-        const actual = addition(text)
-        expect(actual).toEqual(expected)
-    });
+        it('Input 1plus2plus3minus4 Should be 2', () => {
+            const expected = "2"
+            const text = "1plus2plus3minus4"
+
+            const actual = addition(text)
+            expect(actual).toEqual(expected)
+        });
+    })
+
+    describe('Split text to number', () => {
+        it('Split string 1plus2plus3plus4 shoud be array number', () => {
+            const expected = [1, 2, 3, 4]
+            const text = "1plus2plus3plus4"
+
+            const actual = splitTextToNumber(text)
+
+            expect(actual).toEqual(expected)
+        });
+    })
 });
--- src/addition.ts
+++ src/addition.ts
@@ -1,8 +1,12 @@
 export default function addition(text: string):string{
-    const numbers = text.split("plus").map((number) => parseInt(number))
     let sum = 0
+    let numbers = splitTextToNumber(text)
     for(const number of numbers){
         sum += number
     }
     return sum.toString()
+}
+
+export function splitTextToNumber(text: string):number[]{
+    return text.split("plus").map((number) => parseInt(number))
 }
